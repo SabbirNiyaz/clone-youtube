9845a61242878980973acfb6f85229219f9b93e2
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-10-13 00:03:02 +0600

    Complete

--- script/video.js
+++ script/video.js
@@ -13,35 +13,54 @@ const loadCategories = () => {
 }
 
 // create loadVideos
-const loadVideos = () => {
+const loadVideos = (searchText = "") => {
     // console.log("done")
     //fetch the data
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res => res.json())
     .then(data => displayVideos(data.videos))
     .catch((error) => console.log(error));
+}
+
+// Filter category videos
+const loadCategoryVideos = (id) => {
+  // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    // .then(data => console.log(data.category))
+    .then(data => {
+      removeActiveClass();
+      const activeBtn = document.getElementById(`btn-${id}`);
+      // console.log(activeBtn);
+      activeBtn.classList.add("active");
+      displayVideos(data.category);
+    })
+    .catch((error) => console.log(error));
+
 }
 
 // Object { category_id: "1001", category: "Music" }
 // category: "Music"
 // category_id: "1001"
 
-// create displayCategories
+// create displayCategories buttons
 const displayCategories = (categories) => {
         const categoryContainer = document.getElementById('categories');
         categories.forEach( (item) => {
-        console.log(item);
-
+        // console.log(item);
         // create a button
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
-
-        //add button to category container
-        categoryContainer.append(button);
-        
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = `
+        <button id="btn-${item.category_id}" 
+        onclick="loadCategoryVideos(${item.category_id})" 
+        class="btn category-btn rounded-sm">
+          ${item.category}
+        </button>
+        `
+        categoryContainer.append(buttonContainer);
     });
 }
+
 //Video times 
 function getTimeString(time) {
   const date = parseInt(time / 86400);
@@ -58,34 +77,80 @@ function getTimeString(time) {
   }
 }
 
+//Remove active class
+const removeActiveClass = () => {
+  const buttons = document.getElementsByClassName("category-btn");
+  for (let btn of buttons){
+    btn.classList.remove("active");
+  }
+
+}
+
+// Load details
+const loadDetails = async (videoID) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoID}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data);
+  displayDetails(data.video);
+}
+
+const displayDetails = (video) => {
+  // console.log(video.description);
+  const detailsContainer = document.getElementById('model-content');
+  detailsContainer.innerHTML = `
+  <p class="px-2 pt-2">${video.description}</p>
+  `
+  // way-1
+  // document.getElementById('showModalData').click();
+
+  // way-2
+  document.getElementById('my_modal_5').showModal();
+}
 
 // create displayVideos
-const cardDemo = {
+// const cardDemo = {
     
-  "category_id": "1001",
-  "video_id": "aaaa",
-  "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-  "title": "Shape of You",
-  "authors": [
-    {
-      "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-      "profile_name": "Olivia Mitchell",
-      "verified": ""
-    }
-  ],
-  "others": {
-    "views": "100K",
-    "posted_date": "16278"
-  },
-  "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}
+//   "category_id": "1001",
+//   "video_id": "aaaa",
+//   "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//   "title": "Shape of You",
+//   "authors": [
+//     {
+//       "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//       "profile_name": "Olivia Mitchell",
+//       "verified": ""
+//     }
+//   ],
+//   "others": {
+//     "views": "100K",
+//     "posted_date": "16278"
+//   },
+//   "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
 
 
 const displayVideos = (videos) => {
-
         const videosContainer = document.getElementById('videos');
+        videosContainer.innerHTML = "";
+
+        if(videos.length == 0){
+          videosContainer.classList.remove("grid");
+          videosContainer.innerHTML = `
+          <div class="min-h-[600px] flex flex-col gap-5 justify-center
+          items-center">
+          <img src="assets/Icon.png" alt="">
+          <h2 class="text-2xl font-bold text-center">
+          Oops!! Sorry, There is no <br> content here
+          </h2>
+          </div>
+          `
+        } else {
+          videosContainer.classList.add("grid");
+        }
+
         videos.forEach( (video) => {
-        console.log(video);
+        // console.log(video);
 
         // create card
         const card = document.createElement('div');
@@ -97,7 +162,8 @@ const displayVideos = (videos) => {
       alt="video" />
       ${
         video.others.posted_date?.length == 0 ?
-        '' : `<span class="absolute right-2 bottom-2 bg-black p-1 text-white">
+        '' : `<span class="absolute right-2 bottom-2 bg-black p-1 text-white
+         text-xs rounded-sm">
               ${getTimeString(video.others.posted_date)} </span>`
       }
 
@@ -119,13 +185,22 @@ const displayVideos = (videos) => {
         
         </div>
 
-        <p></p>
+        <p class="pt-1">
+        <button onclick="loadDetails('${video.video_id}')" class="btn btn-sm"> 
+         details
+        </button> 
+        </p>
      </div>
     </div>
    `;
     videosContainer.append(card);
     });
 }
+
+document.getElementById('search-input').addEventListener('keyup', (e) => {
+  // console.log(e.target.value);
+  loadVideos(e.target.value);
+})
 
 loadCategories();
 loadVideos();
